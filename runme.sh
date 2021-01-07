ef69a2d0f520bad25928ca5bfca1f6e2c26caefd
ayunami2000 <spwilliamsiam@gmail.com>
2021-01-07 10:01:51 -0500

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -5,4 +5,4 @@
 wget -O jre.tar.gz https://javadl.oracle.com/webapps/download/GetFile/1.8.0_271-b09/61ae65e088624f5aaa0b1d2d801acb16/linux-i586/jre-8u271-linux-i586.tar.gz
 tar -xvf jre.tar.gz
 wget https://raw.githubusercontent.com/mindstorm38/portablemc/master/portablemc.py
-yes y | python3.7 portablemc.py --java ./jre-8u271-linux-i586/bin/java -u virtbruh -v 1.2.5
+yes y | python3.7 portablemc.py --java ./jre1.8.0_271/bin/java -u virtbruh -v 1.2.5
